23f24d66cf4a36eec0052f39a38dc9ae8da3b487
Lina <linabuin@gmail.com>
2023-03-08 08:05:30 +0200

    Exercise8: sum

---
+++ src/Exercise8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercise8 {
+
+    public static void main(String[] args) {
+        System.out.println("calculator");
+        System.out.println("calculating difference of two numbers");
+        System.out.println("provide first number:");
+        int firstNumber;
+        int secondNumber;
+
+        Scanner inputReader = new Scanner(System.in);
+        firstNumber = inputReader.nextInt();
+        System.out.println("Provide second number: ");
+        secondNumber = inputReader.nextInt();
+        int difference = firstNumber - secondNumber;
+        System.out.println("Difference is: " + difference);
+
+        }
+
+}
