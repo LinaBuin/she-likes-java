6b5c77467db826b10915028fcd53b07bdc1ffe63
Lina <linabuin@gmail.com>
2023-03-10 18:31:06 +0200

    Exercise9

---
+++ src/com/sda/she_likes_java/homework/Exercise9.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.homework;
+
+import java.util.Scanner;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+
+        System.out.println("Bouncer In Dicso!!");
+        System.out.println("Entrance over 16 years old");
+        System.out.println("***************");
+
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Tell me how old are You");
+        int personAge = inputReader.nextInt();
+        if (personAge >= 16) {
+            System.out.println("Wellcome");
+        } else {
+            System.out.println("I'm calling your mom");
+        }
+    }
+}
