908aef357d023d5032a84e464f9f477861cefe9c
Lina <linabuin@gmail.com>
2023-03-24 09:35:40 +0200

    IncrementationDecrementationExamples

---
+++ src/com/sda/she_likes_java/operators/IncrementationDecrementationExamples.java
@@ -0,0 +1,23 @@
+package com.sda.she_likes_java.operators;
+
+
+    public class IncrementationDecrementationExamples {
+
+        public static void main(String[] args) {
+            int one = 10;
+            int two = 10;
+
+            one++; // postincrementation
+            two--; // postdecrementation
+
+            System.out.println("one value is: " + one);
+            System.out.println("two value is: " + two);
+
+            ++one; // preincrementation
+            --two; // predecrementation
+            System.out.println("one value is: " + one);
+            System.out.println("two value is: " + two);
+
+        }
+    }
+
