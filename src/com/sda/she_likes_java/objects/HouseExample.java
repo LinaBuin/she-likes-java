c024c5c18f3d8d3fb26d5d664891bb263111eb18
Lina <linabuin@gmail.com>
2023-03-31 21:39:24 +0300

    House example - with shared house object and floors

--- src/com/sda/she_likes_java/objects/HouseExample.java
+++ src/com/sda/she_likes_java/objects/HouseExample.java
@@ -4,6 +4,7 @@ public class HouseExample {
 
     public static void main(String[] args) {
         House myHouse = new House(4, 2);
+        myHouse.setNumberOfFloors(2);
         System.out.println("I've got new house: " + myHouse);
 
         System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
@@ -13,14 +14,13 @@ public class HouseExample {
         System.out.println("Hey, I've got more rooms now...");
         System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
 
-        System.out.println("****************************************************");
-
+        System.out.println("=======================================");
         HouseOwner wife = new HouseOwner("Mary", myHouse);
         HouseOwner husband = new HouseOwner("Jack", myHouse);
 
         System.out.println("Mary would like to repair her house");
-        wife.getHouse().setNumberOfBathrooms(3); // House house = wife.getHouse(); house.setNumberOfBathroom(3);
-        System.out.println("currently the house looks like this: " + myHouse);
+        wife.getHouse().setNumberOfBathrooms(3); // House house = wife.getHouse(); house.setNumberOfBathrooms(3);
+        System.out.println("Currently the house looks like this: " + myHouse);
 
         husband.getHouse().setNumberOfBathrooms(1);
         System.out.println("Currently the house looks like this: " + myHouse);
@@ -28,7 +28,16 @@ public class HouseExample {
         System.out.println("Mary: " + wife);
         System.out.println("Jack: " + husband);
 
+//        String haha = new House(1, 2);
+        myHouse = new House(1, 1);
+        System.out.println("Now myHouse reference: " + myHouse);
+        System.out.println("Mary: " + wife);
+        System.out.println("Jack: " + husband);
 
-
+        myHouse = new House(10, 10);
+        wife.setHouse(myHouse);
+        System.out.println("Mary: " + wife);
+        System.out.println("Jack: " + husband);
+        wife.setHouse(husband.getHouse());
     }
 }
