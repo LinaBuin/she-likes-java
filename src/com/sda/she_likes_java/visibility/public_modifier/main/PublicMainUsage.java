5d09080783fae2965863b085f3ffbf02260da8b5
Lina <linabuin@gmail.com>
2023-04-19 19:52:20 +0300

    public modifier intro

---
+++ src/com/sda/she_likes_java/visibility/public_modifier/main/PublicMainUsage.java
@@ -0,0 +1,15 @@
+package com.sda.she_likes_java.visibility.public_modifier.main;
+
+
+import com.sda.she_likes_java.visibility.public_modifier.drivers.ProfessionalDriver;
+
+public class PublicMainUsage {
+
+    public static void main(String[] args) {
+
+        // because ProfessionalDriver class is public we are able to use it
+        ProfessionalDriver me = new ProfessionalDriver();
+
+    }
+
+}
