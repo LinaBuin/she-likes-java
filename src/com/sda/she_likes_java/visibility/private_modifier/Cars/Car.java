5f14106f17aa317d5b019501abb55cc93e980449
Lina <linabuin@gmail.com>
2023-04-19 20:54:12 +0300

    modifiers on methods

--- src/com/sda/she_likes_java/visibility/private_modifier/Cars/Car.java
+++ src/com/sda/she_likes_java/visibility/private_modifier/Cars/Car.java
@@ -1,7 +1,27 @@
 package com.sda.she_likes_java.visibility.private_modifier.Cars;
+
+//it snor possible to
 class Car {
     private String brand;
     public String model;
 
     String colour;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
 }
+
