3554a0ef8c0df835ddf4f4c5ed29de7b82de4caa
Lina <linabuin@gmail.com>
2023-04-19 19:15:24 +0300

    no major changes

---
+++ src/com/sda/she_likes_java/homework/book/StreamingSong.java
@@ -0,0 +1,14 @@
+package com.sda.she_likes_java.homework.book;
+
+public class StreamingSong {
+    String title;
+    String artist;
+    int duration;
+
+    //void play() {
+        //System.out.println("playing song");}
+
+        void printDetails() {
+            System.out.println("This is " + title + " by " + artist);
+        }
+    }
---
+++ src/com/sda/she_likes_java/homework/book/StreamingSongTestDrive.java
@@ -0,0 +1,11 @@
+package com.sda.she_likes_java.homework.book;
+
+public class StreamingSongTestDrive {
+    public static void main(String[] args) {
+        StreamingSong song = new StreamingSong();
+        song.artist = "The Beatles";
+        song.title = "Come Together";
+        song.printDetails();
+
+    }
+}
