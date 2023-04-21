b0d9bcf0041933343090eebb55838ee92e710055
Lina <linabuin@gmail.com>
2023-04-21 20:36:17 +0300

    String builder

--- src/com/sda/she_likes_java/strings/ImmutableString.java
+++ src/com/sda/she_likes_java/strings/ImmutableString.java
@@ -1,36 +1,29 @@
 package com.sda.she_likes_java.strings;
-
 public class ImmutableString {
     public static void main(String[] args) {
-
-        String name = "Lina";
-        name = name.concat("p");// + is shortcut for concat method call
-        name = name.concat("is 40");
-
-
-        System.out.println("My name now: " +name);
-
-        StringBuilder nameWithBuilder = new StringBuilder("Lina");
-        nameWithBuilder.append("bark");
-        nameWithBuilder.append("bark");
-        nameWithBuilder.append("bark");
-        nameWithBuilder.append("bark");
-        nameWithBuilder.append("bark");
-        System.out.println("name woth string builder" + nameWithBuilder);
-
-        String some = "alise" + "is" + "10" + "yeras";
-
-
+        String name = "mariusz";
+        // name + "pasutuszka"
+        name.concat("pastuszka"); // + is shortcut for concat method call
+        name = name.concat("he’s 40");
+                System.out.println("My name now: " + name);
+        StringBuilder nameWithBuilder = new StringBuilder("mariusz");
+        nameWithBuilder.append(" pastuszka");
+        nameWithBuilder.append(" pastuszka");
+        nameWithBuilder.append(" pastuszka");
+        nameWithBuilder.append(" pastuszka");
+        nameWithBuilder.append(" pastuszka");
+        nameWithBuilder.append(" pastuszka");
+        System.out.println("Name with builder: " + nameWithBuilder);
+        String some = "Alice " + " is" + " 10" + " years";
+        // "Alice is" + " 10" + " years"
+        // "Alice is 10" + years"
+        // Alice is 10 years
+        String alice = "Alice ";
         StringBuilder someString = new StringBuilder();
-        someString.append("Alise");
+        someString.append("Alice");
         someString.append("is");
         someString.append("10");
         someString.append("years");
-
-
-
-
-
-
     }
 }
+
