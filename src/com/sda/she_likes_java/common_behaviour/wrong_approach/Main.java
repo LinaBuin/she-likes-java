ec17cabde5a4656f7f39372733a9d4ed425948f9
Lina <linabuin@gmail.com>
2023-04-26 21:29:10 +0300

    movable interface introduced

--- src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
+++ src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
@@ -11,9 +11,28 @@ public class Main {
         Bird parrot = new Parrot();
         Bird ostrich = new Ostrich();
 
+        Superman superSuperMan = new Superman();
+        Flyable superFly = superSuperMan;
+        Swimmable superSwim = superSuperMan;
+        superSuperMan.fly();
+        superSuperMan.swim();
+
         Flyable superman = new Superman();
         Flyable flyableParrot = new Parrot();
+
+        Swimmable tiger2 = (Swimmable) tiger;
+        Swimmable swimmableTiger = new Tiger();
+
+        // Tiger assignment ways
+        Tiger newTiger = new Tiger();
+        newTiger.go();
+        newTiger.swim();
+        Animal animalTiger = new Tiger();
+        animalTiger.go();
+        Swimmable swimmableTiger2 = new Tiger();
+        swimmableTiger2.swim();
+        Object objectTiger = new Tiger();
+        // cannot call go() and swim() - thery are not visible here
+
+        }
     }
-}
-
-
--- src/com/sda/she_likes_java/common_behaviour/wrong_approach/Superman.java
+++ src/com/sda/she_likes_java/common_behaviour/wrong_approach/Superman.java
@@ -1,14 +1,17 @@
 package com.sda.she_likes_java.common_behaviour.wrong_approach;
-public class Superman implements Flyable, Swimmable {
+public class Superman implements Flyable, Swimmable, Moveable {
     @Override
     public void fly() {
         System.out.println("Superman is flying!!!");
-
     }
 
     @Override
     public void swim() {
-        System.out.println("superman is super swimmer");
+        System.out.println("Superman is super swimmer");
+    }
 
+    @Override
+    public void move() {
+        System.out.println("Superman is moving super fast");
     }
 }
