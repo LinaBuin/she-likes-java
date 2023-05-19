40aaa45ed5616dea6e785b9197c8f3ee0bbfa491
Lina <linabuin@gmail.com>
2023-05-19 21:12:23 +0300

    Compound comparator with methods provided by java reversed, then Comparing

--- src/com/sda/she_likes_java/sorting/SortingPersonsMain.java
+++ src/com/sda/she_likes_java/sorting/SortingPersonsMain.java
@@ -10,12 +10,16 @@ public class SortingPersonsMain {
         SortedPerson olaf = new SortedPerson("Olaf", 22);
         SortedPerson gustaw = new SortedPerson("Gustaw", 11);
         SortedPerson zorro = new SortedPerson("Zorro", 11);
+        SortedPerson zorro1 = new SortedPerson("Zorro", 7);
+        SortedPerson zorra = new SortedPerson("Zorra", 11);
         List<SortedPerson> persons = new ArrayList<>();
         persons.add(olaf);
         persons.add(gustaw);
         persons.add(zorro);
         persons.add(zorro);
         persons.add(zorro);
+        persons.add(zorra);
+        persons.add(zorro1);
 
         Collections.sort(persons);
         System.out.println("After sorting: " + persons);
@@ -29,17 +33,22 @@ public class SortingPersonsMain {
         System.out.println("Reversed order based on age: " + persons);
 
         List<String> names = new ArrayList<>();
-        names.add("ramune");
+        names.add("Ramune");
         names.add("Lina");
-        names.add("karolina");
+        names.add("Alma");
+        names.add("Karolina");
         Collections.sort(names);
         System.out.println("sorted names: " + names);
 
         Collections.sort(persons, new LexicalOrderForSortedPerson());
-        System.out.println("persosn sorted by name" + persons);
+        System.out.println("persons sorted by name: " + persons);
 
         Collections.sort(persons, new ReversedLexicalOrderForSortedPerson());
-        System.out.println("persons sorted by name  - reversed: " + persons);
+        System.out.println("persons sorted by name - reversed: " + persons);
 
+        Collections.sort(persons,
+                new LexicalOrderForSortedPerson().reversed().thenComparing(reversedAgeOrder));
     }
 }
+
+
