01ec2f60a77e416f5d8cfbae0537fe29c084a9a5
Lina <linabuin@gmail.com>
2023-05-22 18:55:55 +0300

    WIP exercise_27

---
+++ src/com/sda/she_likes_java/homework/exercise_27/Main.java
@@ -0,0 +1,25 @@
+package com.sda.she_likes_java.homework.exercise_27;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyDate newYear = new MyDate(2022, 1, 1);
+        MyDate hisBirthday = new MyDate(1975, 1, 4);
+        MyDate myBirthday = new MyDate(1977, 9, 17);
+        MyDate daySchool = new MyDate(2023, 9, 1);
+
+        List<MyDate> dates = new ArrayList<>();
+        dates.add(newYear);
+        dates.add(hisBirthday);
+        dates.add(myBirthday);
+        dates.add(daySchool);
+
+        Collections.sort(dates);
+        System.out.println("after sorting: " + dates);
+
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/exercise_27/MyDate.java
@@ -0,0 +1,42 @@
+package com.sda.she_likes_java.homework.exercise_27;
+
+public class MyDate implements Comparable<MyDate> {
+
+    private final int year;
+
+    private final int month;
+
+    private final int day;
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                '}';
+    }
+
+    @Override
+    public int compareTo(MyDate o) {
+        return 0;
+    }
+}
