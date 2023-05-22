e0126ababccafa078be0baa584bbf5a9c3074cee
Lina <linabuin@gmail.com>
2023-05-22 19:22:16 +0300

    Final classes and methods explained

---
+++ src/com/sda/she_likes_java/polymorphism/TryToOverrideFinalMethod.java
@@ -0,0 +1,8 @@
+package com.sda.she_likes_java.polymorphism;
+
+public class TryToOverrideFinalMethod
+extends WithFinalMethod
+
+{
+    //public final void notToOverride()
+}
---
+++ src/com/sda/she_likes_java/polymorphism/WithFinalMethod.java
@@ -0,0 +1,7 @@
+package com.sda.she_likes_java.polymorphism;
+public class WithFinalMethod {
+
+    public final void notToOverride() {
+
+    }
+}
