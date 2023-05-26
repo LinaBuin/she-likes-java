c5fbb71185a8a52ab33179073335c8d3af59cbb5
Lina <linabuin@gmail.com>
2023-05-26 21:33:46 +0300

    Applicaton for getting data from db almost vorking

--- src/com/sda/she_likes_java/database/DatabaseUtils.java
+++ src/com/sda/she_likes_java/database/DatabaseUtils.java
@@ -1,32 +1,34 @@
 package com.sda.she_likes_java.database;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtils {
 
-
-    /** Script for creating table
-    * CREATE TABLE TEST
-    * (ID INT PRIMARY KEY, NAME VARCHAR(255));
-    *
-    * Script for inserting one record (row) into table
-    * INSERT INTO
-    * TEST(ID, NAME)
-    * VALUES(1, 'Olga');
-
+    /**
+     * Script for creating table
+     * CREATE TABLE TEST
+     * (ID INT PRIMARY KEY, NAME VARCHAR(255));
+     *
+     * Script for inserting one record (row) into table
+     * INSERT INTO
+     * TEST(ID, NAME)
+     * VALUES(1, 'Olga');
+     *
      * Script for reading data
-     * * SELECT ID, NAME
-     * * FROM TEST;
+     * SELECT ID, NAME
+     * FROM TEST;
      *
-     *
+     * Script for updating data
      * UPDATE TEST -- name of table to update
      * SET NAME = 'Alma' -- column value to change
-     * WHERE ID=2; -- selected raw
-    */
-
-
+     * WHERE ID=2; -- selected row
+     *
+     * Script for deleting data
+     * DELETE
+     * FROM TEST
+     * WHERE ID=3;
+     */
     // handle internally
     public static Connection getConnection() {
         try {
@@ -40,8 +42,12 @@ public class DatabaseUtils {
 
     // declare
     public static Connection getConnectionButSometimesThrowException() throws SQLException {
-        return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/databases/jdbc",
+        return DriverManager.getConnection("jdbc:h2:~\\Data\\she-goes-tech\\db",
                 "sa",
-                "");
+                "sa");
     }
 }
+
+
+
+
