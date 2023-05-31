5bade84ba54e2b90d8b1c52a6d28d07d515a1e4d
Lina <linabuin@gmail.com>
2023-05-31 21:33:00 +0300

    Students repository for AccentureStudent

--- src/com/sda/she_likes_java/database_in_memory/Main.java
+++ src/com/sda/she_likes_java/database_in_memory/Main.java
@@ -1,14 +1,22 @@
 package com.sda.she_likes_java.database_in_memory;
-
 import java.sql.Connection;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Connection connection = DbConnectionUtils.getConnection(DbCredentials.h2InMemoryUrl, DbCredentials.h2Username, DbCredentials.h2Password);
+        Connection connection = DbConnectionUtils.getConnection(DbCredentials.h2InMemoryUrl,
+                DbCredentials.h2Username, DbCredentials.h2Password);
 
         System.out.println("Do we have connection? " + (connection != null));
 
         DbInit.prepareData(connection);
+
+        System.out.println("Let's read all students");
+        AccentureStudentsRepository repository = new AccentureStudentsRepository(connection);
+
+        for (AccentureStudent student : repository.getAllStudents()) {
+            System.out.println("Student: " + student);
+        }
     }
 }
